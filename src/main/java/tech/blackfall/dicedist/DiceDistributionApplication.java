2162603890ed62b895bf369b84119ab1ee9bd3a9
Artur Michalak <artur.michalak@circlekeurope.com>
2021-06-27 14:15:15 +0200

    Basic structure and dependencies

---
+++ src/main/java/tech/blackfall/dicedist/DiceDistributionApplication.java
@@ -0,0 +1,12 @@
+package tech.blackfall.dicedist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceDistributionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiceDistributionApplication.class, args);
+  }
+}
